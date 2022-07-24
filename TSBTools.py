e16006821b3934dcbe6c3603b5b50c85d2cab91c
user.email <0kq@0kqnet.work>
2022-07-24 14:29:55 +0900

    インストール機能の実装完了(多分)

--- TSBTools.py
+++ TSBTools.py
@@ -1,5 +1,6 @@
 from PySide2 import QtWidgets,QtGui
 from PySide2.QtCore import QThread,Signal,Qt
+from PySide2.QtGui import QPalette, QColor
 from main_gui import Ui_MainWindow
 from tsb.client import tsbAPI
 from tsb.client import mojangAPI
@@ -7,7 +8,11 @@ import markdown
 import os
 import requests
 import zipfile
-
+import nbt2yaml
+import json
+import uuid
+import datetime
+import base64
 
 tsb = tsbAPI()
 version = "0.1"
@@ -24,7 +29,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.thread.signal.connect(self.show_tsb_releases)
         self.thread.start()
 
-        #self.progressBar.setValue(0)
         title_md = f"""
 <h1 style=\"text-align: center;\">
     TSBTools v{version}
@@ -32,10 +36,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 <h3 style=\"text-align: center;\">
     Tool created by 0kq 
     <br><br>
-    <a href=\"https://twitter.com/_0kq_\">
+    <a style=\"text-decoration:none;\" href=\"https://twitter.com/_0kq_\">
         <img src=\"{os.getcwd()}\\assets\\twitter.png\" alt=\"Twitter\" width=32 height=26>
     </a> 
-    <a href=\"https://github.com/0kq-github\">
+    <a style=\"text-decoration:none;\" href=\"https://github.com/0kq-github\">
         <img src=\"{os.getcwd()}\\assets\\github.png\" alt=\"GitHub\" width=32 height=32>
     </a>
     <br><br>
@@ -66,12 +70,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.lineEdit.setText(path)
 
     def install_client(self):
-        if not self.comboBox.currentText():
-            QtWidgets.QMessageBox.information(self,"TSBTools","バージョンが選択されていません")
+        if not self.check_input():
             return
-        if not self.lineEdit.text():
-            QtWidgets.QMessageBox.information(self,"TSBTools","ゲームディレクトリが指定されていません")
-            return
+        ret = QtWidgets.QMessageBox.information(self,"TSBTools","起動構成を作成しますか？",QtWidgets.QMessageBox.Yes,QtWidgets.QMessageBox.No)
+        self.install()
+        if ret == QtWidgets.QMessageBox.Yes:
+            self.create_profile()
+        mcversion = self.check_world_version(self.lineEdit.text()+"\\saves\\"+f"TheSkyBlessing_{self.comboBox.currentText()}"+"\\level.dat")
+        QtWidgets.QMessageBox.information(self,"TSBTools",f"のインストールが完了しました！\nTSB {self.comboBox.currentText()}\nMinecraft {mcversion}")
+
+    def install(self):
         download_url = tsb.releases[self.comboBox.currentText()]["download_url"]
         prog = QtWidgets.QProgressDialog(self)
         prog.setWindowModality(Qt.ApplicationModal)
@@ -80,7 +88,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         prog.setFixedHeight(100)
         prog.setValue(0)
         prog.show()
-        #print(download_url)
         file_size = tsb.releases[self.comboBox.currentText()]["size"]
         res = requests.get(download_url,stream=True)
         i = 0
@@ -103,13 +110,51 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         with zipfile.ZipFile(zip_path) as zf:
             files = zf.namelist()
             prog.setMaximum(len(files))
+            ext_path = self.lineEdit.text()+"\\saves\\"+f"TheSkyBlessing_{self.comboBox.currentText()}"
             for p in files:
-                zf.extract(p,self.lineEdit.text()+"/saves/"+f"TheSkyBlessing_{self.comboBox.currentText()}")
+                zf.extract(p,ext_path)
                 i += 1
                 prog.setValue(i)
                 prog.setLabelText(p)
         prog.close()
-        QtWidgets.QMessageBox.information(self,"TSBTools",f"TSB {self.comboBox.currentText()}のインストールが完了しました！")
+
+    def check_world_version(self,nbt_path):
+        with open(nbt_path,"rb") as f:
+            nbt= nbt2yaml.parse_nbt(f)
+            for n in nbt.data[0].data:
+                if n.name == "Version":
+                    for nm in n.data:
+                        if nm.name == "Name":
+                            return nm.data
+
+    def check_input(self):
+        if not self.comboBox.currentText():
+            QtWidgets.QMessageBox.information(self,"TSBTools","バージョンが選択されていません")
+            return False
+        if not self.lineEdit.text():
+            QtWidgets.QMessageBox.information(self,"TSBTools","ゲームディレクトリが指定されていません")
+            return False
+        return True
+
+    def create_profile(self):
+        if not self.check_input():
+            return
+        launcher_profiles = os.environ["appdata"]+"\\.minecraft\\launcher_profiles.json"
+        profile_uuid = uuid.uuid4().hex
+        mcversion = self.check_world_version(self.lineEdit.text()+"\\saves\\"+f"TheSkyBlessing_{self.comboBox.currentText()}"+"\\level.dat")
+        with open(launcher_profiles,mode="r") as f:
+            profiles = json.load(f)
+            profiles["profiles"][profile_uuid] = {
+                "created": datetime.datetime.utcnow().isoformat()+"Z",
+                "gameDir": self.lineEdit.text(),
+                "icon":"data:image/png;base64,"+base64.b64encode(open(os.getcwd()+"\\assets\\tsb_icon.png","rb").read()).decode("utf-8"),
+                "javaArgs" : "-Xmx4G -Xms4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M",
+                "lastVersionId": mcversion,
+                "name":"TheSkyBlessing "+self.comboBox.currentText(),
+                "type":"custom"
+            }
+        with open(launcher_profiles,mode="w") as f:
+            json.dump(profiles,f,indent=2)
 
 
 
@@ -140,8 +185,29 @@ class load_mc_versions(QThread):
         except:
             pass
 
-# アプリの実行と終了
+
 app = QtWidgets.QApplication()
+
+#ダークテーマ
+"""
+app.setStyle("Windows")
+palette = QPalette()
+palette.setColor(QPalette.Window, QColor(53, 53, 53))
+palette.setColor(QPalette.WindowText, Qt.white)
+palette.setColor(QPalette.Base, QColor(25, 25, 25))
+palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+palette.setColor(QPalette.ToolTipBase, Qt.black)
+palette.setColor(QPalette.ToolTipText, Qt.white)
+palette.setColor(QPalette.Text, Qt.white)
+palette.setColor(QPalette.Button, QColor(53, 53, 53))
+palette.setColor(QPalette.ButtonText, Qt.white)
+palette.setColor(QPalette.BrightText, Qt.red)
+palette.setColor(QPalette.Link, QColor(42, 130, 218))
+palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+palette.setColor(QPalette.HighlightedText, Qt.black)
+app.setPalette(palette)
+"""
+
 window = MainWindow()
 window.show()
 app.exec_()
