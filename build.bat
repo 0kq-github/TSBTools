855c5dd058cb62dca4a7a10d7a4670efb3992676
user.email <0kq@0kqnet.work>
2022-12-09 16:53:15 +0900

    fix 0.1.6

--- build.bat
+++
@@ -1,3 +0,0 @@
-:pyinstaller TSBTools.py --noconsole --onefile --icon=tsb.ico
-:pyarmor pack -e " --noconsole --onefile --icon=tsb.ico" TSBTools.py
-python -m PyInstaller TSBTools.py --noconsole --onefile --icon=tsb.ico
--- updater.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-with open("test.py",mode="r") as f:
-  old = f.read()
-os.remove("test.py")
-with open("test.py",mode="w") as f:
-  f.write("print(\"Updated!\")\nexit()\n"+old)
-subprocess.Popen(["python","test.py"])
